fa5db8a289fc18158302d296cec4d5ba12250463
ivan.nascimento <ivan.nascimento@foton.la>
2021-06-13 11:57:51 -0300

    h1/comunicar-api testar conexão

--- app/src/main/java/com/example/caruapp/MainActivity.kt
+++ app/src/main/java/com/example/caruapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.caruapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
+        val repo = PokemonRepository()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val pokes = repo.get1stGen()
+
+            val poke = pokes[4]
+
+            runOnUiThread {
+                findViewById<TextView>(R.id.text).text = poke.name
+            }
+        }
+
+        // TODO: Chamar a lista de pokemon e exibir num recyclerView
     }
 }
