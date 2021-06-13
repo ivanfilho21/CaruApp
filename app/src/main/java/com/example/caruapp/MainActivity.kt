9868915c4e299154fe8933d4f803e6f906953585
ivan.nascimento <ivan.nascimento@foton.la>
2021-06-13 15:12:39 -0300

    h1/comunicar-api utilizar corrotina para desobstruir a thread principal

--- app/src/main/java/com/example/caruapp/MainActivity.kt
+++ app/src/main/java/com/example/caruapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.caruapp
 
 import android.os.Bundle
-import android.os.StrictMode
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
